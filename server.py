55626e64a022fa302db0b677b1677cdafa5eaf2b
alice <alice21022007@mail.ru>
2024-04-26 00:02:17 +0300

    вроде готово))

--- server.py
+++ server.py
@@ -6,10 +6,8 @@ import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup
 
-import json
-
-from tg_bot.ORM_test.data import db_session
-from tg_bot.ORM_test.data.users import User
+from ORM_test.data import db_session
+from ORM_test.data.users import User
 
 db_session.global_init("ORM_test/db/Users.db")
 
@@ -21,8 +19,10 @@ load_dotenv()
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-reply_keyboard = [["/photo_of_the_Earth"], ["/photo_Mars"], ["/photo_NASA"], ["/start"]]
+reply_keyboard = [["/photo_of_the_Earth"], ["/photo_Mars"], ["/photo_NASA"], ["/start"], ["/stop"], ['/sun_sistem'],
+                  ["/Info_Mars"], ["/zayac"]]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
 
 
 async def stop(update, context):
@@ -43,12 +43,10 @@ async def first_N_response(update, context):
     user = update.effective_user
     locality = update.message.text
     d = dt.datetime.now().date()
-    s = []
     db_sess = db_session.create_session()
     username = user['username']
     func = 'photo_NASA'
     date = ''.join(locality)
-    print(username, func, date)
     user = User(username=username, date=date, func=func)
 
     db_sess.add(user)
@@ -56,35 +54,31 @@ async def first_N_response(update, context):
     api_key = 'iuCdE8es7d2DuclaVnHviPHbWC8fRT21VfnAykJT'
     url = f'https://api.nasa.gov/planetary/apod?date={locality}&api_key={api_key}'
     file = get(url).json()
-    if 'msg' in file:
-        await update.message.reply_text('alisa lox nasa')
+    if len(locality.split('-')) != 3:
+        await update.message.reply_text('Неверный формат даты, повторите попытку')
+        return
     else:
+        s = locality.split('-')
+    if s[0].isdigit() and s[1].isdigit() and s[2].isdigit():
+        if (2013 < int(s[0]) < d.year) and (1 <= int(s[1]) <= 12) and (1 <= int(s[2]) <= 31):
+            if len(file) != 0:
+                if len(file) != 1:
+                    for num in file:
+                        if num == 'url':
+                            url = file['url']
+                            await update.message.reply_photo(url)
+        elif (s[0] == d.year) and (s[1] <= d.month) and (s[1] < d.day):
+            await update.message.reply_photo(url)
 
-        for num in file:
-            if num == 'url':
-                url = file['url']
-                await update.message.reply_photo(url)
 
-
-    # if len(locality.split('-')) != 3:
-    #     await update.message.reply_text('Неверный формат даты, повторите попытку3')
-    # else:
-    #     s = locality.split('-')
-    # if s[0].isdigit() and s[1].isdigit() and s[2].isdigit():
-    #     if (2013 < int(s[0]) < d.year) and (1 <= int(s[1]) <= 12) and (1 <= int(s[2]) <= 31):
-    #         if len(file) != 0:
-    #             if len(file) != 1:
-    #                 for num in file:
-    #                     if num == 'url':
-    #                         url = file['url']
-    #         await update.message.reply_photo(url)
-    #     elif (s[0] == d.year) and (s[1] <= d.month) and (s[1] <= d.day):
-    #         await update.message.reply_photo(url)
-    #     else:
-    #         await update.message.reply_text('Неверный формат даты, повторите попытку1')
-    #         print(update.message.reply_photo(url))
-    # else:
-    #     await update.message.reply_text('Неверный формат даты, повторите попытку2')
+        else:
+            await update.message.reply_text('Извините, у NASA пока нет фотографии сделанной в этот день)')
+            return
+    else:
+        await update.message.reply_text('Проверьте верность формата введённой даты')
+        return
+    await update.message.reply_text('Вау, какое красивое фото!\n'
+                                    'Попробуйте наши другие функции или запустите снова эту))')
     return ConversationHandler.END
 
 
@@ -104,7 +98,6 @@ async def photo_of_the_Earth(update, context):
     return 3
 
 
-
 async def first_E_response(update, context):
     global username
     global date
@@ -115,45 +108,39 @@ async def first_E_response(update, context):
     username = user['username']
     func = 'photo_of_the_Earth'
     date = ''.join(locality)
-    # print(username, func, date)
     user = User(username=username, date=date, func=func)
 
     db_sess.add(user)
     db_sess.commit()
-    s = locality.split('-')
     d = dt.datetime.now().date()
-    s = []
     api_key = 'iuCdE8es7d2DuclaVnHviPHbWC8fRT21VfnAykJT'
     url_1 = f'https://api.nasa.gov/EPIC/api/natural/date/{locality}?api_key={api_key}'
-    print(url_1)
-    try:
-        file = get(url_1).json()
-
-    except :
-        await update.message.reply_text('alisa lox earth')
+    if len(locality.split('-')) != 3:
+        await update.message.reply_text('Неверный формат даты, повторите попытку')
+        return
     else:
-        file = get(url_1).json()
         s = date.split('-')
-        url_2 = file[0]['identifier']
-        url_3 = f'https://api.nasa.gov/EPIC/archive/natural/{s[0]}/{s[1]}/{s[2]}/png/epic_1b_{url_2}.png?api_key={api_key}'
-        await update.message.reply_photo(url_3)
-    # if len(locality.split('-')) != 3:
-    #     await update.message.reply_text('Неверный формат даты, повторите попытку3')
-    # else:
-    #     s = date.split('-')
-    # if s[0].isdigit() and s[1].isdigit() and s[2].isdigit():
-    #     if (2010 < int(s[0]) < d.year) and (1 <= int(s[1]) <= 12) and (1 <= int(s[2]) <= 31):
-    #         url_2 = file[0]['identifier']
-    #         url_3 = f'https://api.nasa.gov/EPIC/archive/natural/{s[0]}/{s[1]}/{s[2]}/png/epic_1b_{url_2}.png?api_key={api_key}'
-    #         await update.message.reply_photo(url_3)
-    #     elif (s[0] == d.year) and (s[1] <= d.month) and (s[1] <= d.day):
-    #         url_2 = file[0]['identifier']
-    #         url_3 = f'https://api.nasa.gov/EPIC/archive/natural/{s[0]}/{s[1]}/{s[2]}/png/epic_1b_{url_2}.png?api_key={api_key}'
-    #         await update.message.reply_photo(url_3)
-    #     else:
-    #         await update.message.reply_text('Неверный формат даты, повторите попытку1')
-    # else:
-    #     await update.message.reply_text('Неверный формат даты, повторите попытку2')
+    if s[0].isdigit() and s[1].isdigit() and s[2].isdigit():
+        if (2010 < int(s[0]) < d.year) and (1 <= int(s[1]) <= 12) and (1 <= int(s[2]) <= 31):
+            file = get(url_1).json()
+            url_2 = file[0]['identifier']
+            url_3 = f'https://api.nasa.gov/EPIC/archive/natural/{s[0]}/{s[1]}/{s[2]}/png/epic_1b_{url_2}.png?api_key={api_key}'
+            await update.message.reply_photo(url_3)
+
+        elif (s[0] == d.year) and (s[1] <= d.month) and (s[1] < d.day):
+            file = get(url_1).json()
+            url_2 = file[0]['identifier']
+            url_3 = f'https://api.nasa.gov/EPIC/archive/natural/{s[0]}/{s[1]}/{s[2]}/png/epic_1b_{url_2}.png?api_key={api_key}'
+            await update.message.reply_photo(url_3)
+
+        else:
+            await update.message.reply_text('Извините, у NASA пока нет фотографии сделанной в этот день)')
+            return
+    else:
+        await update.message.reply_text('Проверьте верность формата введённой даты')
+        return
+    await update.message.reply_text('Наша планета прекрасна!!\n'
+                                    'Попробуйте наши другие функции или запустите снова эту))')
     return ConversationHandler.END
 
 
@@ -168,7 +155,7 @@ conv_handler2 = ConversationHandler(
 
 
 async def photo_Mars(update, context):
-    await update.message.reply_html('Введите дату которая вас интересует (в формате гггг-м-д),'
+    await update.message.reply_html('Введите дату которая вас интересует (в формате гггг-мм-дд),'
                                     ' и мы отправим вам фото, сделанное марсоходом в этот день!')
     return 5
 
@@ -191,32 +178,30 @@ async def first_M_response(update, context):
 
     api_key = 'iuCdE8es7d2DuclaVnHviPHbWC8fRT21VfnAykJT'
     url = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={locality}&api_key={api_key}'
-    try:
-        file = get(url).json()
-    except:
-        await update.message.reply_text('alisa lox mars')
-    else:
-        file = get(url).json()
-        await update.message.reply_photo(file['photos'][1]['img_src'])
 
-    s = locality.split('-')
     d = dt.datetime.now().date()
-    s = []
-    # if len(locality.split('-')) != 3:
-    #     await update.message.reply_text('Неверный формат даты, повторите попытку')
-    # else:
-    #     s = locality.split('-')
-    # print(s)
-    # print(update.message.reply_photo(file['photos'][1]['img_src']))
-    # if s[0].isdigit() and s[1].isdigit() and s[2].isdigit():
-    #     if (2010 < int(s[0]) < d.year) and (1 <= int(s[1]) <= 12) and (1 <= int(s[2]) <= 31):
-    #         await update.message.reply_photo(file['photos'][1]['img_src'])
-    #     elif (s[0] == d.year) and (s[1] <= d.month) and (s[1] <= d.day):
-    #         await update.message.reply_photo(file['photos'][1]['img_src'])
-    #     else:
-    #         await update.message.reply_text('Неверный формат даты, повторите попытку1')
-    # else:
-    #     await update.message.reply_text('Неверный формат даты, повторите попытку2')
+    if len(locality.split('-')) != 3:
+        await update.message.reply_text('Неверный формат даты, повторите попытку')
+        return
+    else:
+        s = locality.split('-')
+    if s[0].isdigit() and s[1].isdigit() and s[2].isdigit():
+        if (2010 < int(s[0]) < d.year) and (1 <= int(s[1]) <= 12) and (1 <= int(s[2]) <= 31):
+            file = get(url).json()
+            await update.message.reply_photo(file['photos'][1]['img_src'])
+
+        elif (s[0] == d.year) and (s[1] <= d.month) and (s[1] <= d.day):
+            file = get(url).json()
+            await update.message.reply_photo(file['photos'][1]['img_src'])
+
+        else:
+            await update.message.reply_text('Извините, у NASA пока нет фотографии сделанной в этот день)')
+            return
+    else:
+        await update.message.reply_text('Проверьте верность формата введённой даты')
+        return
+    await update.message.reply_text('Марс иснтересная планета, неправдали?\n'
+                                    'Попробуйте наши другие функции или запустите снова эту))')
     return ConversationHandler.END
 
 
@@ -228,6 +213,24 @@ conv_handler3 = ConversationHandler(
 
     },
     fallbacks=[CommandHandler('stop', stop)])
+
+
+async def sun_sistem(update, context):
+    await update.message.reply_html(
+        'Что такое Солнечная система? Солнечная система – это совокупность планет, вращающихся вокруг центральной звезды.\n'
+        ' Ученым удалось установить, что ей примерно 4,57 млрд лет, а появилась она за счет гравитационного сжатия газопылевого облака. В основе системы лежит яркая звезда – Солнце, которое удерживает планеты и другие объекты, заставляя их вращаться по орбите на определенном расстоянии.\n'
+        'Оно во много раз превосходит по диаметру другие объекты, находящиеся в области его притяжения. Интересный факт: Солнце обладает такой большой массой, что все остальные планеты системы составляют лишь 0,0014% от его веса. В составе Солнечной системы, помимо звезды, находится восемь основных планет, а также пять карликовых. Располагается она в галактике Млечный Путь, в рукаве Ориона. \n'
+        'Сейчас процесс возникновения Солнечной системы описывается следующими шагами:\n'
+        'Изначально в этой области вселенной находилось облако, состоящее из гелия, водорода и других веществ, полученных при взрывах старых звезд. В небольшой его части началось уплотнение, ставшее центром гравитационного коллапса. '
+        'Он постепенно начал притягивать к себе окружающие вещества.\n'
+        'Из-за притяжения веществ размеры облака начали уменьшаться, при этом росла скорость вращения. '
+        'Постепенно его форма превратилась в диск.\n'
+        'По мере сжатия увеличивалась плотность частиц на единицу объема, что приводило к постепенному нагреву вещества за счет частых столкновений молекул.\n'
+        'Когда центр гравитационного коллапса разогрелся до нескольких тысяч кельвинов, он начал светиться, что означало образование протозвезды. '
+        'Параллельно с этим, в разных областях диска начали появляться другие уплотнения, которые в будущем послужат гравитационными центрами для образования планет.\n '
+        'Финальный этап формирования солнечной системы начался в период, когда температура центра протозвезды превысила несколько миллионов кельвинов. '
+        'Тогда гелий и водород вступили в реакцию термоядерного синтеза, что привело к появлению полноценной звезды. '
+        'Остальные уплотнения диска постепенно сформировались в планеты, которые начали вращаться в одном направлении вокруг Солнца, находясь на одной плоскости.')
 
 
 async def start(update, context):
@@ -277,24 +280,23 @@ async def start(update, context):
             rf'Привет,{user.mention_html()}! Рады видеть тебя снова. Выбери интересующую функцию)' rf'',
             reply_markup=markup)
 
-
-async def data(update, funcc):
-    date = update.message.text
-    await update.message.reply_text(f'Ваша функция:{funcc}\n Ваша дата {date}')  # перевести объект тг в текст
-
+async def zayac(update, cotext):
+    await update.message.reply_photo('zayac.jpg')
 
 def main():
     token = os.environ.get('TOKEN', '')
     app = Application.builder().token(token).build()
     app.add_handler(conv_handler)
     app.add_handler(conv_handler2)
-    app.add_handler(conv_handler3)  ###размер кнопок  текст на руском?
+    app.add_handler(conv_handler3)
 
     app.add_handler(CommandHandler('start', start))
     app.add_handler(CommandHandler('photo_NASA', photo_NASA))
     app.add_handler(CommandHandler('photo_of_the_Earth', photo_of_the_Earth))
     app.add_handler(CommandHandler('photo_Mars', photo_Mars))
     app.add_handler(CommandHandler('stop', stop))
+    app.add_handler(CommandHandler('sun_sistem', sun_sistem))
+    app.add_handler(CommandHandler('zayac', zayac))
 
     app.run_polling()
 
