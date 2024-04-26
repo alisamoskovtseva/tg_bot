6b830db3bf4c8976f5448cc49e3e07ae2b42fb9d
alice <alice21022007@mail.ru>
2024-04-26 16:14:38 +0300

    ГОТОВОООО))

--- server.py
+++ server.py
@@ -6,9 +6,8 @@ import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup
 
-from tg_bot.ORM_test.data import db_session
-from tg_bot.ORM_test.data.users import User
-
+from ORM_test.data import db_session
+from ORM_test.data.users import User
 
 db_session.global_init("ORM_test/db/Users.db")
 
@@ -20,9 +19,10 @@ load_dotenv()
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-reply_keyboard = [["/photo_of_the_Earth"], ["/photo_Mars"], ["/photo_NASA"],  ['/Sun_sistem'],
-                  ["/Info_func"], ["/zayac"],["/start"], ["/stop"]]
+reply_keyboard = [["/photo_of_the_Earth"], ["/photo_Mars"], ["/photo_NASA"], ['/Sun_sistem'],
+                  ["/Info_func"], ["/zayac"], ["/start"], ["/stop"]]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
 
 async def Info_func(update, context):
     await update.message.reply_html('<b>/photo_of_the_Earth</b> - '
@@ -39,7 +39,94 @@ async def Info_func(update, context):
                                     "При нажатии этой кнопке Вы просто начнете сначало")
     await update.message.reply_html('<b>/stop</b> '
                                     "При нажатии этой кнопки бот остановится")
+    await update.message.reply_html('Planet - '
+                                    'информация про планеты нашей солнечной системы:\n'
+                                    '<b>/Venus</b> - '
+                                    'информация про Венеру\n'
+                                    '<b>/The_Earth </b> - '
+                                    'информация про Землю\n'
+                                    '<b>/Mars</b> - '
+                                    'информация про Марс\n'
+                                    '<b>/Jupiter</b> - '
+                                    'информация про Юпитер\n'
+                                    '<b>/Saturn</b> - '
+                                    'информация про Сатурн\n'
+                                    '<b>/Uranus</b> - '
+                                    'информация про Сатурн\n'
+                                    '<b>/Neptune </b> - '
+                                    'информация про Нептун\n'
+                                    )
     await update.message.reply_html('Выберите одну из представленных функций')
+
+
+async def Mercury(update, context):
+    await update.message.reply_html(
+        'Ближайшая к Солнцу планета, также является самой маленькой в системе. Ее радиус составляет лишь 2440 км.'
+        ' Свое название она получила в честь бога торговли Меркурия. Ее поверхность серого цвета, из-за чего многие сравнивают с Луной.'
+        ' Планета не содержит спутников, а из-за сильных солнечных ветров ее атмосфера практически полностью разряжена')
+
+
+async def Venus(update, context):
+    await update.message.reply_html(
+        'Вторая планета от Солнца, носит имя в честь древнеримской богини любви.\n'
+        'Отличительными особенностями являются отсутствие естественных спутников и высокое содержание углекислого газа в атмосфере.\n'
+        'Радиус Венеры практически совпадает с земным: 6051 км, что всего лишь на 5% меньше.'
+        'Из-за этого планеты называют “сестрами”. Однако внешне Венера сильно отличается, представляя собой шар молочного цвета.'
+        'Поверхность практически полностью состоит из застывшей лавы с редкими кратерами от метеоритов.')
+
+
+async def The_Earth(update, context):
+    await update.message.reply_html(
+        'Ближайшая к Солнцу планета, также является самой маленькой в системе. Ее радиус составляет лишь 2440 км.'
+        ' Свое название она получила в честь бога торговли Меркурия. Ее поверхность серого цвета, из-за чего многие сравнивают с Луной.'
+        ' Планета не содержит спутников, а из-за сильных солнечных ветров ее атмосфера практически полностью разряжена')
+
+
+async def Mars(update, context):
+    await update.message.reply_html(
+        '“Красная” планета является самой далекой от Солнца, относящейся к земной группе.'
+        ' Также считается самой маленькой после Меркурия.\n'
+        ' Ее радиус составляет 3396 км.\n'
+        ' Поверхность состоит преимущественно из песчаных и земляных рельефов, разбитых на светлые и темные области, именуемые материками и морями соответственно. '
+        'В XXI веке Марс представляет большой интерес для ученых.\n'
+        'Поскольку планета находится в относительной досягаемости, на нее регулярно отправляются марсоходы для сбора данных. ')
+
+
+async def Jupiter(update, context):
+    await update.message.reply_html(
+        'Самая большая планета в Солнечной системе.\n'
+        'Ее радиус составляет 69912 км, что практически в 20 раз превышает земной.\n'
+        'Ученые пока не могут точно определить состав планеты, лишь известно, что в ней больше ксенона, аргона и криптона больше, чем на Солнце.'
+        'Также у Юпитера 67 спутников, причем некоторые по размеру вполне походят на планеты.'
+        'Например, Ганимед на 8% больше, чем Меркурий, а Ио имеет собственную атмосферу.\n '
+        'Также есть теория, что Юпитер должен был стать полноценной звездой, но на этапе развития он так и остался планетой.')
+
+
+async def Saturn(update, context):
+    await update.message.reply_html(
+        'Шестая по счету планета, знаменитая своими кольцами, состоящими из льда и каменистых метеороидов.\n'
+        'Радиус сатурна составляет 57360 км.\n '
+        'Ученые еще не изучили детально состав поверхности, но смогли установить, что в ней имеются практически такие же химические элементы, как и на Солнце.'
+        ' Вокруг Сатурна находятся 62 спутника. \n'
+        'Интересный факт: не так давно было установлено, что помимо Сатурна кольцами обладают и другие газовые гиганты, но они заметны не так сильно.'
+        ' О причинах их появления пока можно лишь догадываться.')
+
+
+async def Uranus(update, context):
+    await update.message.reply_html(
+        'Третья по размерам планета в Солнечной системе. Ее радиус равен 25267 км.\n'
+        'Температура на Уране держится на уровне -230 градусов по Цельсию, что делает его самой холодной планетой.\n'
+        'Также он обладает уникальной особенностью: ось вращения расположена под углом, из-за чего при движении планета производит впечатление катящегося шара.\n'
+        'Поверхность состоит преимущественно из льда, также имеется небольшое количество гелия и водорода.')
+
+
+async def Neptune(update, context):
+    await update.message.reply_html(
+        'Восьмая планета от Солнца была открыта не с помощью наблюдений, а за счет математических расчетов.\n'
+        'Наблюдая аномалии в движении Урана ученые выдвинули предположение, что они возникли из-за наличия еще одного небесного тела больших размеров.\n'
+        'Нептун обладает радиусом в 24547 км.\n'
+        'Поверхность похожа на урановую, но по ней гуляют самые сильные ветра в системе, разгоняющиеся до 260 м/с.')
+
 
 async def stop(update, context):
     await update.message.reply_text("Пока!")
@@ -283,7 +370,6 @@ async def start(update, context):
                                         '<b>•Околоземные астероиды</b>\n'
                                         '<b>•Камера для съёмки Земли</b>')
         await update.message.reply_html('Выберите одну из представленных функций')
-        # МНОГО ТЕКСА?
         await update.message.reply_html('<b>Астрономическая картина дня</b> - '
                                         'Откройте для себя космос! Каждый день'
                                         ' появляется новое изображение или фотография нашей'
@@ -329,6 +415,14 @@ def main():
     app.add_handler(CommandHandler('Info_func', Info_func))
     app.add_handler(CommandHandler('sun_sistem', sun_sistem))
     app.add_handler(CommandHandler('zayac', zayac))
+    app.add_handler(CommandHandler('Mercury', Mercury))
+    app.add_handler(CommandHandler('Venus', Venus))
+    app.add_handler(CommandHandler('The_Earth', The_Earth))
+    app.add_handler(CommandHandler('Mars', Mars))
+    app.add_handler(CommandHandler('Jupiter', Jupiter))
+    app.add_handler(CommandHandler('Saturn', Saturn))
+    app.add_handler(CommandHandler('Uranus', Uranus))
+    app.add_handler(CommandHandler('Neptune', Neptune))
 
     app.run_polling()
 
